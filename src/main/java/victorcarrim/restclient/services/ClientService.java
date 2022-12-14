42bdafaaf68cec53306c8ae80acd061d9cb305f5
victor.gff <victor.gff@pti.org.br>
2022-12-14 14:52:25 -0300

    add readOnly on transactional

--- src/main/java/victorcarrim/restclient/services/ClientService.java
+++ src/main/java/victorcarrim/restclient/services/ClientService.java
@@ -1,10 +1,10 @@
 package victorcarrim.restclient.services;
 
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import victorcarrim.restclient.dto.ClientDTO;
 import victorcarrim.restclient.entities.Client;
 import victorcarrim.restclient.exceptions.ResourceNotFoundException;
@@ -18,14 +18,15 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
-    @Transactional()
+    @Transactional(readOnly = true)
     public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
         Page<Client> listClients = repository.findAll(pageRequest);
         Page<ClientDTO> listDtoClients = listClients.map(x -> new ClientDTO(x));
         return listDtoClients;
     }
 
-    @Transactional()
+
+    @Transactional(readOnly = true)
     public ClientDTO findById(Long id){
         Optional<Client> clientOptional = repository.findById(id);
         Client entity = clientOptional.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
